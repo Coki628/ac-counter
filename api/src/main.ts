e43f9f0083097a51a24c70a49a4eff959ebbf108
Coki <srcn4frdm.c@gmail.com>
2022-12-27 14:44:32 +0900

    removed debug log for port

--- api/src/main.ts
+++ api/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
 
   await app.listen(port);
 
-  console.log(`app is runnning on port ${port}`);
+  // console.log(`app is runnning on port ${port}`);
 }
 bootstrap();
