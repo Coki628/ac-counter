85cd62e55afdf949436d14a241f0e329d1aa28cb
Coki <srcn4frdm.c@gmail.com>
2023-01-30 11:12:29 +0900

    minor changes

--- api/src/types.ts
+++ api/src/types.ts
@@ -2,4 +2,4 @@ export type OnlineJudge = {
   key: string;
   siteName: string;
   counter?: (userName: string) => Promise<number | null> | (number | null);
-}
+};
--- web/src/types.ts
+++ web/src/types.ts
@@ -18,4 +18,4 @@ export type OnlineJudge = {
   key: string;
   siteName: string;
   counter?: (userName: string) => Promise<number | null> | (number | null);
-}
+};
